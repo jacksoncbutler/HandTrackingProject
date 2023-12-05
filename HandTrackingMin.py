20cacef8445f1e66804e174bbdd7440b13b12e1e
jacksoncbutler <jackson@butler.net>
2023-12-05 13:26:52 -0600

    Basic Hand Tracking Complete

---
+++ HandTrackingMin.py
@@ -0,0 +1,22 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(1)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    print(type(img))
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+    if results.multi_hand_landmarks:
+        for hand in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, hand, mpHands.HAND_CONNECTIONS)
+    
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
