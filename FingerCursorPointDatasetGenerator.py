5bc62cb678aef842903932b11c44e8eeb7131bc7
jacksoncbutler <jackson@butler.net>
2023-12-07 21:54:45 -0600

    Adding Dataset, and much improved model performance

--- FingerCursorPointDatasetGenerator.py
+++ FingerCursorPointDatasetGenerator.py
@@ -14,11 +14,15 @@ TEXTCOLOR = (  0,   0,  0)
 
 running = True
 
-dataFile = "/Users/jackcameback/Classes/Fall2023/MachineLearning/HandTrackingProject/data/FingerCursorData.json"
+# print(locations)
+
+dataFile = "/Users/jackcameback/Classes/Fall2023/MachineLearning/HandTrackingProject/data/otherData.json"
 data = {"0":[], "1":[], "dot":[]}
+
 
 def main():
     global running, screen
+    
     
     pygame.init()
     screen = pygame.display.set_mode((width, height))
@@ -31,27 +35,42 @@ def main():
     currentTime = 0
     detector = HandDetector()
     
+    record = False
+
+    locations = []
+    delay = 0.25
+    ptime = time.time()
+
+    for y in range(0, height, 40):
+        for x in range(0, width, 40):
+            locations.append((x,y))
+    
     while running:
         success, img = cap.read()
         detector.img = img
-        detector.detect_hands(draw=False)
+        detector.detect_hands(draw=True)
         detector.find_node_positions_of_hand(draw=False)
+
+        if record:
+            ctime = time.time()
+            if (ctime - ptime) > delay:
+                print("recording")
+                handPos = detector.get_positions(-1)
+                recordData(pos, handPos)
+                ptime = ctime
+        currentTime = time.time()
+        fps = 1/(currentTime-prevTime)
+        prevTime = currentTime
+        
+        cv2.putText(detector.img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         
         
-        # currentTime = time.time()
-        # fps = 1/(currentTime-prevTime)
-        # prevTime = currentTime
         
-        # cv2.putText(detector.img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-        
-        
-        
-        # cv2.imshow("Image", detector.img)
+        cv2.imshow("Image", detector.img)
         cv2.waitKey(1) 
         
         ev = pygame.event.get()
 
-        
         for event in ev:
             if event.type == pygame.KEYDOWN:
                 pass
@@ -61,7 +80,23 @@ def main():
                     recordData(pos, handPos)
                     pos = (random.randint(0,width),random.randint(0,height))
                     drawCircle(pos)
+                
+                if event.key == pygame.K_n:
+                    print("next")
+                    if len(locations) > 0:
+                        pos = locations.pop()
+                        drawCircle(pos)
+                        
+                if event.key == pygame.K_s:
+                    print('S key')
+                    if record:
+                        print("STOP RECORDING")
+                        record = False
+                    else:
+                        record = True
+                
                 if event.key == pygame.K_ESCAPE:
+                    print("SAVED")
                     with open(dataFile, "w") as file:
                         json.dump(data, file)
             if event.type == pygame.QUIT:
@@ -73,6 +108,7 @@ def drawCircle(pos):
     pygame.display.update()
 
 def recordData(pos, handPos):
+    
     data["0"].append(handPos[0])
     data["1"].append(handPos[1])
     data["dot"].append(pos)
